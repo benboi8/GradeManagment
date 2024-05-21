75703c99de613c863585136a7b04ef80ee4d5bca
benboi8 <benboi294@gmail.com>
2024-05-21 19:30:20 +0100

    First Commit

---
+++ grades.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version metadata for the gradereport_gradingmanager plugin.
+ *
+ * @package   gradereport_gradingmanager
+ * @copyright 2024, Ben Williams <benboi294@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../../config.php');
+
+
+$PAGE->set_url(new moodle_url('/grade/report/gradingmanager/grades.php'));
+$PAGE->set_context(\context_system::instance());
+$PAGE->set_title('Grades');
+
+
+$templatecontext = (object)[
+
+];
+
+
+echo $OUTPUT->header();
+
+echo $OUTPUT->render_from_template('gradereport_gradingmanager/grades', $templatecontext);
+
+echo $OUTPUT->footer();
